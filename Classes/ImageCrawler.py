eab123f73f05044314fc0a2632af8b98e484dd5b
Benjamin Purtzer <reptilebuyer@hotmail.com>
2020-07-19 20:49:36 -0700

    Added library

--- Classes/ImageCrawler.py
+++ Classes/ImageCrawler.py
@@ -3,6 +3,7 @@ import re
 from urllib.parse import urlparse    
 import os
 import html
+from bs4 import BeautifulSoup
 
 class ImageCrawler:
     def __init__(self, starting_url, transformer, dataLimitCount, recursionLimit):
@@ -24,36 +25,42 @@ class ImageCrawler:
       self.data = set()
         
     def start(self, badVisited = 0, url=''):
-      print(f'Starting at: {url}')
-      if (not (self.recursionCount > self.recursionLimit | len(self.data) > self.dataLimitCount | url in self.visited)):
-        raw = requests.get(self.starting_url if len(url) == 0 else url)
-        raw = raw.text
-        htmlString = html.unescape(raw)
-        transformed = self.transformer(htmlString)
-        if (len(transformed) == 0 & badVisited > 10):
-          return
+      destination = self.starting_url if len(url) == 0 else url
+      if ((self.recursionCount < self.recursionLimit) & (len(self.data) < self.dataLimitCount) & (destination not in self.visited)):
+        print(f'Going to: {destination}')
+        self.visited.add(destination)
+        raw = None
+        try:
+          raw = requests.get(destination)
+        except:
+          print(f'Broken link: {url}')
+        if (raw):
+          raw = raw.text
+          bs_obj = BeautifulSoup(raw)
 
-        self.data = self.data.union(transformed)
-        links = self.getLinks(htmlString)
-        for x in links:
-          self.start(0 if len(transformed) > 0 else badVisited+1, x)
+          transformed = self.transformer(bs_obj)
+          if (len(transformed) == 0 & badVisited > 10):
+            return
 
-    def getLinks(self, text):
-      text = html.unescape(text)
-      aTags = re.findall('<a[^<]*</a>', text)
-      print(f'TAGS: {len(set(aTags))}')
-      sourcesRaw = map(lambda x: re.search('href="[^"]*"', x), aTags)
-      print(sourcesRaw)
-      sourcesMid = set()
+          self.data = self.data.union(transformed)
+          links = self.getLinks(bs_obj)
+          for x in links:
+            self.start(0 if len(transformed) > 0 else badVisited+1, x)
+      else:
+        print(f'Stopping work with COUNT: {self.recursionCount} and DATA: {len(self.data)}')
+
+    # Working
+    def getLinks(self, bs_obj):
+      try:
+        hrefs = map(lambda x: x.attrs.get('href', '') if x.attrs else '', bs_obj.find_all('a'))
+      except:
+        print('Encountered Error')
       sourcesRefined = set()
-      print(sourcesRaw)
-      for x in sourcesRaw:
-          a = x.group()
-          sourcesMid.add(a)
-      print(sourcesMid)
-      for x in sourcesMid:
-          a = x[6:-1]
-          sourcesRefined.add(a)
-      sourcesRefined = filter(lambda x: re.search("https", x) != None, sourcesRefined)
-      print(f'Links found: {sourcesRefined}')
+      for x in hrefs:
+        b = re.search('http[^"]*', x)
+        if (b):
+          c = b.group()
+          d = re.search('google', c)
+          if (d == None):
+            sourcesRefined.add(c)
       return sourcesRefined
--- Main.py
+++ Main.py
@@ -1,26 +1,14 @@
 from Classes import ImageCrawler
 import re
 
-def transformer(htmlText):
-  imageTags = re.findall('<img[^>]*>', htmlText)
-  sourcesRaw = map(lambda x: re.search('src="[^"]*"', x), imageTags)
-  sourcesMid = []
-  sourcesRefined = []
-  for x in sourcesRaw:
-      a = x.group()
-      sourcesMid.append(a)
-  # print(sourcesMid)
-  for x in sourcesMid:
-      a = x[5:-1]
-      sourcesRefined.append(a)
-  sourcesRefined = filter(lambda x: re.search("https", x) != None, sourcesRefined)
-  sourcesRefined = set(sourcesRefined)
-  # print(f'FOUND {sourcesRefined}')
+def transformer(bs_obj):
+  sources = map(lambda x: x.attrs.get('src', '') if x.attrs else '', bs_obj.find_all('img'))
+  sourcesRefined = set(filter(lambda x: re.search("https", x) != None, sources))
+  print(f'Images found: {sourcesRefined}')
   return sourcesRefined
 
 def main():
-  ic = ImageCrawler("https://www.google.com/search?q=moldy+buds&oq=moldy+buds&aqs=chrome.0.69i59l2j0l5j69i60.3027j1j4&sourceid=chrome&ie=UTF-8", transformer, 20, 20)
-  print("hello")
+  ic = ImageCrawler("https://www.google.com/search?q=moldy+buds&oq=moldy+buds&aqs=chrome.0.69i59l2j0l5j69i60.7018j1j7&sourceid=chrome&ie=UTF-8", transformer, 20, 20)
   ic.start()
   print(ic.data)
 
